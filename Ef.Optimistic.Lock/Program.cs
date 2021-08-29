1861a2dec46aeddf58bce58860dabf699c87ba5c
narojay <hj200812@126.com>
2021-08-29 14:43:56 +0800

    Fix

--- Ef.Optimistic.Lock/Program.cs
+++ Ef.Optimistic.Lock/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ef.Optimistic.Lock
 {
@@ -7,18 +8,21 @@ namespace Ef.Optimistic.Lock
     {
         static void Main(string[] args)
         {
-            using (var context = new OfficeContext()){
-                try
+            using var context = new OfficeContext();
+            try
+            {
+                var a = context.Students.FirstOrDefault(x => x.Id == 1);
+                if (a is not null)
                 {
-                    var a = context.Students.FirstOrDefault(x => x.Id == 1); 
+                    context.Entry(a).State = EntityState.Modified;
                     a.Name = "1234";
                     var c = context.SaveChanges();
                 }
-                catch(Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-         
+              
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
